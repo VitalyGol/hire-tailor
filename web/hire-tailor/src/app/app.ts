60dac1600f7b5276011a6496c601d17f990853c8
VitalyGol <69786847+VitalyGol@users.noreply.github.com>
2026-05-15 09:13:46 +0300

    feat: add spinner service and interceptor for loading indication (#46)

--- web/hire-tailor/src/app/app.ts
+++ web/hire-tailor/src/app/app.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { SpinnerService } from './services/spinner.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {}
+export class App {
+  private readonly spinner = inject(SpinnerService);
+
+  protected readonly spinnerVisible = this.spinner.visible;
+}
