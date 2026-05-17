b9b874f74f3322348dc4803699893f3cfabb053c
VitalyGol <simpleindifficult@gmail.com>
2026-05-17 12:05:11 +0300

    fix: update version number in get_resume_prompt method

--- server/service/prompt_builder.py
+++ server/service/prompt_builder.py
@@ -2,7 +2,7 @@ class PromptBuilder:
     def get_resume_prompt(self, language: str, job_requirement: str, resume: str):
         prompt = {
             "id": "pmpt_6a0422fe89208194a3a61dc00ecab910066104b9e7dc11a6",
-            "version": "9",
+            "version": "11",
             "variables": {
             "resume": resume,
             "job_requirements": job_requirement,
